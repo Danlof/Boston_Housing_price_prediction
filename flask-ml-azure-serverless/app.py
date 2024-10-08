7b9ba58aaf15a304cf6cf78eea590956442aa400
danlof <danmusyoki.dk@gmail.com>
2024-10-08 13:36:18 +0300

    app

---
+++ flask-ml-azure-serverless/app.py
@@ -0,0 +1,79 @@
+from flask import Flask, request, jsonify
+from flask.logging import create_logger
+import logging
+import traceback
+
+import pandas as pd
+import joblib
+from sklearn.preprocessing import StandardScaler
+
+app = Flask(__name__)
+LOG = create_logger(app)
+LOG.setLevel(logging.INFO)
+
+
+def scale(payload):
+    """Scales Payload"""
+
+    LOG.info("Scaling Payload: %s", payload)
+    scaler = StandardScaler().fit(payload)
+    scaled_adhoc_predict = scaler.transform(payload)
+    return scaled_adhoc_predict
+
+
+@app.route("/")
+def home():
+    html = (
+        "<h3>Boston Housing Price Prediction Home: From Azure Pipelines (Continuous Delivery)</h3>"
+    )
+    return html.format(format)
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    """Performs a prediction
+
+    input looks like:
+            {
+    "CHAS":{
+      "0":0
+    },
+    "RM":{
+      "0":6.575
+    },
+    "TAX":{
+      "0":296.0
+    },
+    "PTRATIO":{
+       "0":15.3
+    },
+    "B":{
+       "0":396.9
+    },
+    "LSTAT":{
+       "0":4.98
+    }
+
+    result looks like:
+    { "prediction": [ 20.35373177134412 ] }
+
+    """
+
+    try:
+        xgb = joblib.load("xgb_housing_model.joblib")
+    except Exception as e:
+        LOG.error("Error loading model: %s", str(e))
+        LOG.error("Exception traceback: %s", traceback.format_exc())
+        return "Model not loaded"
+
+    json_payload = request.json
+    LOG.info("JSON payload: %s", json_payload)
+    inference_payload = pd.DataFrame(json_payload)
+    LOG.info("inference payload DataFrame: %s", inference_payload)
+    scaled_payload = scale(inference_payload)
+    prediction = list(xgb.predict(scaled_payload))
+    return jsonify({"prediction": prediction})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
